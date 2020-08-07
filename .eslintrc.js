a7c3300cf28b99da6f8af11497ddc90934df50e1
Carlos <carlosacorti@hotmail.com>
2020-08-07 18:45:59 +0200

    initial config
    -on .eslintrc.js: linter config usisng babel for react
    -on .prettierrc.js: prettier extra settings (single cuotes)
    -on package.json: babel and linter dependencies
    -on .npmrc: save-exact=true

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['main.js', 'node_modules/'],
+
+  rules: {
+    'react/prop-types': ['off'],
+    'react/display-name': ['off'],
+    'react/no-unescaped-entities': ['off'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-console': [
+      'warn',
+      { allow: ['clear', 'info', 'error', 'dir', 'trace'] },
+    ],
+    curly: 'error',
+    'no-else-return': 'error',
+    'no-unneeded-ternary': 'error',
+    'no-useless-return': 'error',
+    'no-var': 'error',
+    'one-var': ['error', 'never'],
+    'prefer-arrow-callback': 'error',
+    strict: 'error',
+    'symbol-description': 'error',
+    yoda: ['error', 'never', { exceptRange: true }],
+  },
+};
