a93c7ed73f309916c887362e6457d73f4722bcb3
Carlos <carlosacorti@hotmail.com>
2020-08-07 22:30:10 +0200

    setting up webpack
    -on webpack.config.js: creates main and vedor files for css and js, and
    also creates assets.json with data about files created, all stored in
    public folder
    -on lib/server.js: sets statics files on public folder, '/' route
    renders index.ejs with assets.json data
    -on lib/views/index.ejs: scripts load main.js and vendor.js
    from assets.json data
    -on lib/components/App.js: returns jsx to test webpack config
    -on lib/renderers/domRender.js: refactoring folder name
    -on lib/renderers/serverRenderer.js: refactoring folder name

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const AssetsPlugin = require('assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtraxtPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: {
+    main: './lib/renderers/domRender.js',
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'public', 'build'),
+    filename: isDev ? '[name].js' : '[name].[contentHash].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss|css)$/,
+        use: [MiniCssExtraxtPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new AssetsPlugin({
+      filename: 'assets.json',
+      path: path.join(__dirname, 'public'),
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtraxtPlugin({
+      filename: 'css-[name].[contentHash].css',
+    }),
+  ],
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'initial',
+          name: 'vendor',
+          enforce: true,
+        },
+      },
+    },
+  },
+};
